a1398dc07a2902493249d29df13670e8c2c0cdce
ViratReddy <viratvits@gmail.com>
2023-01-10 18:12:42 +0000

    wrote my first python selenium automation script

--- main.py
+++ main.py
@@ -1,8 +1,26 @@
-import pandas as pd
+from selenium import webdriver
 
 
-data = {'name': ['nick', 'david', 'joe', 'ross','root'],
-		'age': ['5', '10', '7', '6','11']}
-new = pd.DataFrame.from_dict(data)
+def get_driver():
+  #set options to make browsing easier
+  options = webdriver.ChromeOptions()
+  options.add_argument('disable-infobars')
+  options.add_argument('start-maximized')
+  options.add_argument('disable-dev-shm-usage')
+  options.add_argument('no-sandbox')
+  options.add_experimental_option('excludeSwitches', ['enable-automation'])
+  options.add_argument('disable-blink-features=AutomationControlled')
 
-print(new)
+  driver = webdriver.Chrome(options=options)
+  driver.get("https://automated.pythonanywhere.com/")
+  return driver
+
+
+def main():
+  driver = get_driver()
+  element = driver.find_element(by="xpath",
+                                value="/html/body/div[1]/div/h1[1]")
+  return element.text
+
+
+print(main())
